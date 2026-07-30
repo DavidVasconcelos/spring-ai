b17b212ef50627dc288ed42da7902e4441cabd78
David Vasconcelos <dsv10@hotmail.com>
2026-07-29 21:29:47 -0300

    copy from section-2

---
+++ section-4/spring-ai/src/main/java/com/eazybytes/springai/configuration/ChatClientConfiguration.java
@@ -0,0 +1,72 @@
+package com.eazybytes.springai.configuration;
+
+import com.eazybytes.springai.advisor.TokenUsageAuditAdvisor;
+import java.util.List;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfiguration {
+
+  private static final String DEFAULT_USER = "How can you help me?";
+  private static final List<String> SENSITIVE_WORDS = List.of("Shakira", "Madonna", "Argentina");
+
+  @Bean
+  public ChatClient defaultChatClient(ChatClient.Builder chatClientBuilder) {
+    return chatClientBuilder
+        .defaultUser(DEFAULT_USER)
+        .defaultAdvisors(List.of(new SafeGuardAdvisor(SENSITIVE_WORDS), new SimpleLoggerAdvisor(),
+            new TokenUsageAuditAdvisor()))
+        .build();
+  }
+
+  @Bean
+  public ChatClient hrChatClient(ChatClient.Builder chatClientBuilder) {
+    return chatClientBuilder
+        .defaultSystem("""
+            You are an internal HR assistant. Your role is to help\s
+            employees with questions related to HR policies, such as\s
+            leave policies, working hours, benefits, and code of conduct.
+            If a user asks for help with anything outside of these topics,\s
+            kindly inform them that you can only assist with queries related to\s
+            HR policies.
+            """
+        )
+        .defaultAdvisors(new SimpleLoggerAdvisor())
+        .defaultUser(DEFAULT_USER)
+        .build();
+  }
+
+  @Bean
+  public ChatClient helpDeskChatClient(ChatClient.Builder chatClientBuilder) {
+    return chatClientBuilder
+        .defaultSystem("""
+            You are an internal IT helpdesk assistant. Your role is to assist 
+            employees with IT-related issues such as resetting passwords, 
+            unlocking accounts, and answering questions related to IT policies.
+            If a user requests help with anything outside of these 
+            responsibilities, respond politely and inform them that you are 
+            only able to assist with IT support tasks within your defined scope.
+            """
+        )
+        .defaultAdvisors(new TokenUsageAuditAdvisor())
+        .defaultUser(DEFAULT_USER)
+        .build();
+  }
+
+  @Bean
+  public ChatClient customerServiceClient(ChatClient.Builder chatClientBuilder) {
+    return chatClientBuilder
+        .defaultSystem("""
+            You are a professional customer service assistant which helps drafting email
+            responses to improve the productivity of the customer support team
+            """
+        )
+        .defaultAdvisors(new TokenUsageAuditAdvisor())
+        .defaultUser(DEFAULT_USER)
+        .build();
+  }
+}
