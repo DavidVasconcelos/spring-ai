f5cb9c9a6c385789b4d3663cbd4db01b5c71c50e
David Vasconcelos <dsv10@hotmail.com>
2026-07-29 22:37:51 -0300

    chat conversation id

--- section-4/spring-ai/src/main/java/com/eazybytes/springai/configuration/ChatMemoryClientConfig.java
+++ section-4/spring-ai/src/main/java/com/eazybytes/springai/configuration/ChatMemoryClientConfig.java
@@ -1,6 +1,9 @@
 package com.eazybytes.springai.configuration;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class ChatMemoryClientConfig {
 
   @Bean("chatMemoryChatClient")
-  public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+  public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    SimpleLoggerAdvisor loggerAdvisor = new SimpleLoggerAdvisor();
+    MessageChatMemoryAdvisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory)
+        .build();
+
     return chatClientBuilder
+        .defaultAdvisors(loggerAdvisor, memoryAdvisor)
         .build();
   }
 }
--- section-4/spring-ai/src/main/java/com/eazybytes/springai/controller/ChatMemoryController.java
+++ section-4/spring-ai/src/main/java/com/eazybytes/springai/controller/ChatMemoryController.java
@@ -1,9 +1,11 @@
 package com.eazybytes.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,13 @@ public class ChatMemoryController {
   }
 
   @GetMapping("/chat-memory")
-  public ResponseEntity<String> chatMemory(@RequestParam("message") String message) {
-    return ResponseEntity.ok(chatClient.prompt().user(message).call().content());
+  public ResponseEntity<String> chatMemory(@RequestHeader("username") String username,
+      @RequestParam("message") String message) {
+    return ResponseEntity.ok(chatClient
+        .prompt()
+        .user(message)
+        .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, username))
+        .call()
+        .content());
   }
 }
