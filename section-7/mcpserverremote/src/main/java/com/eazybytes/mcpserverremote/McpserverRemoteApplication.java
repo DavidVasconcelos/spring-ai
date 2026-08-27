defd05a960578a9c5a951d7064df1dd50028a09f
David Vasconcelos <dsv10@hotmail.com>
2026-08-27 20:53:46 -0300

    Rename main application and test classes to McpserverRemoteApplication and configure server port 8090
    Rename McpserverremoteApplication and its test to McpserverRemoteApplication/McpserverRemoteApplicationTests and update the SpringApplication.run call accordingly to match the new class name. Add server.port: 8090 to application.yaml to explicitly configure the application's HTTP port.

--- section-7/mcpserverremote/src/main/java/com/eazybytes/mcpserverremote/McpserverRemoteApplication.java
+++ section-7/mcpserverremote/src/main/java/com/eazybytes/mcpserverremote/McpserverRemoteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class McpserverremoteApplication {
+public class McpserverRemoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(McpserverremoteApplication.class, args);
+		SpringApplication.run(McpserverRemoteApplication.class, args);
 	}
 
 }
